3cbfcce9d4ef53226cea0f1c9f94d2d9e69ff9f9
Philipp Siebers <philipp.siebers@accenture.com>
2023-12-08 11:35:16 +0100

    accidentals test and tempo test

--- MicroPython/play_anthem.py
+++ MicroPython/play_anthem.py
@@ -4,5 +4,6 @@ buzzer = robot.Buzzer()
 display = robot.Display()
 
 def play():
-    intro = "t140 l2 v10 ms2c8c8c8a8g8f8m2c msc8c8c8a8g8f8md ms8c8c8c8b8a8g8>c8>c8>c8>c8>d8>c8-b8g8m2f"
+    intro = "t140 l2 v10 msc8c8c8a8g8f8mc msc8c8c8a8g8f8 l2md ms8c8c8c8b8a8g8>c8>c8>c8>c8>d8>c8b8g8m2f"
+    intro = "t140 l2 v10 b -b b- b b8 b16 b32 b2 b1"
     buzzer.play(intro)
