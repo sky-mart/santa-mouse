96ce7d3476df49ff7798913d32289fa906fd797f
Philipp Siebers <philipp.siebers@accenture.com>
2023-12-08 11:30:53 +0100

    test

--- MicroPython/play_anthem.py
+++ MicroPython/play_anthem.py
@@ -4,5 +4,5 @@ buzzer = robot.Buzzer()
 display = robot.Display()
 
 def play():
-    intro = "t140 l2 v10 ms2c8c8c8a8g8f8m2c msc8c8c8a8g8f8m2d ms8c8c8c8b8a8g8>c8>c8>c8>c8>d8>c8-b8g8m2f"
+    intro = "t140 l2 v10 ms2c8c8c8a8g8f8m2c msc8c8c8a8g8f8md ms8c8c8c8b8a8g8>c8>c8>c8>c8>d8>c8-b8g8m2f"
     buzzer.play(intro)
