eb69ec02f89e073be77f7d4cbb2bff544a846f9d
Vlad Agievich <uladzislau.ahiyevich@esrlabs.com>
2023-12-08 16:05:22 +0100

    Distance measurement with one sensor

--- MicroPython/distance_sensing.py
+++ MicroPython/distance_sensing.py
@@ -2,11 +2,9 @@ from pololu_3pi_2040_robot import robot
 from machine import I2C, Pin
 from vl6180x import Sensor
 import time
+
 display = robot.Display()
 
-
-i2c = I2C(id=0, scl=Pin(5), sda=Pin(4), freq=400_000)
-tof = Sensor(i2c)
 MAX_DIST_RAW = 190
 MIN_DIST_RAW = 10
 MAX_DIST_PHY = 20.5
@@ -15,13 +13,27 @@ RAW_RANGE = 180
 PHY_RANGE = 19
 FACTOR = 180.0 / 19.0
 
-def get_distance():
-  time.sleep(0.01)
-  actual_value = tof.range()
-  if actual_value > 190:
-    actual_value = 190
-  actual_value -= MIN_DIST_RAW
-  return (actual_value / FACTOR) + 1.5
+
+def dbg(text):
+  display.fill(0)
+  display.text(text, 0, 0)
+  display.show()
+
+class DistanceSensor:
+  def __init__(self, i2c):
+    self.tof = Sensor(i2c)
+
+  def get_distance(self):
+    time.sleep(0.01)
+    actual_value = self.tof.range()
+    if actual_value > 190:
+      actual_value = 190
+    actual_value -= MIN_DIST_RAW
+    return (actual_value / FACTOR) + 1.5
+
+
+i2c = I2C(id=0, scl=Pin(5), sda=Pin(4), freq=400_000)
+ds = DistanceSensor(i2c)
 
 while True:
-  get_distance()
+  dbg(str(ds.get_distance()))
