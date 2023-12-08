e01dbe34b333b5449daef6e56d861eea7c408324
Philipp Siebers <philipp.siebers@accenture.com>
2023-12-08 11:26:00 +0100

    new time

--- MicroPython/play_anthem.py
+++ MicroPython/play_anthem.py
@@ -4,5 +4,5 @@ buzzer = robot.Buzzer()
 display = robot.Display()
 
 def play():
-    intro = "t120 l2 v10 ms8c8c8c8a8g8f2mc ms8c8c8c8a8g8f2md ms8c8c8c8b8a8g8>c8>c8>c8>c8>d8>c8b8g2mf"
+    intro = "t120 l2 v10 msc8c8c8a8g8f8mc2 msc8c8c8a8g8f8md2 ms8c8c8c8b8a8g8>c8>c8>c8>c8>d8>c8b8g8mf2"
     buzzer.play(intro)
