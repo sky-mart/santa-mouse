114a0f407de9110821f3c2c3d94d81f222cc8a49
Philipp Siebers <philipp.siebers@accenture.com>
2023-12-08 11:55:10 +0100

    Music change

--- MicroPython/play_anthem.py
+++ MicroPython/play_anthem.py
@@ -4,6 +4,5 @@ buzzer = robot.Buzzer()
 display = robot.Display()
 
 def play():
-    intro = "t140 l2 v10 msc8c8c8a8g8f8mc msc8c8c8a8g8f8 l2md ms8c8c8c8b8a8g8>c8>c8>c8>c8>d8>c8b8g8m2f"
-    intro = "t140 l2 v10 b -b b- b b8 b16 b32 b2 b1"
+    intro = "t140 l2 v10 msc8c8c8a8g8f8mlc msc8c8c8a8g8f8mld2 msc8c8c8b8a8g8>c8>c8>c8>c8>d8>c8b8g8mlf4"
     buzzer.play(intro)
