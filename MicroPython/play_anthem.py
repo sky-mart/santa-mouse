b4be2695dbf9d39fc020ca43ca4eedd7aeae3cfe
Philipp Siebers <philipp.siebers@accenture.com>
2023-12-08 12:01:22 +0100

    Muasic

--- MicroPython/play_anthem.py
+++ MicroPython/play_anthem.py
@@ -4,5 +4,5 @@ buzzer = robot.Buzzer()
 display = robot.Display()
 
 def play():
-    intro = "t140 l2 v10 msc8c8c8a8g8f8mlc msc8c8c8a8g8f8mld2 msc8c8c8b8a8g8>c8>c8>c8>c8>d8>c8b8g8mlf4"
+    intro = "t140 l2 v10 msc8c8c8a8g8f8mlc msc8c8c8a8g8f8mld msc8c8c8b8a8g8>c8>c8>c8>c8>d8>c8b8g8mlf2"
     buzzer.play(intro)
