de17e10c44fd7972d8bb2da58b4213e29673857b
Philipp Siebers <philipp.siebers@accenture.com>
2023-12-08 10:29:47 +0100

    Jingle Bells?

--- MicroPython/play_anthem.py
+++ MicroPython/play_anthem.py
@@ -4,5 +4,5 @@ buzzer = robot.Buzzer()
 display = robot.Display()
 
 def play():
-    intro = "t240 cccagfc4 cccagfd4 cccbagc>c>c>c>d>c>bfd4"
+    intro = "t240 cccagf+lc8 cccagf+ld8 cccbag>c>c>c>c>d>cbf+ld8"
     buzzer.play(intro)
